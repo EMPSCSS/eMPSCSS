527ef022554d6e0f80280989440f9dba29c79016
EMPSCSS <empscss@gmail.com>
2014-05-16 12:21:26 +1200

    include header HTML

---
+++ javascripts/include-html-sections.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+
+    $('header').append('<div class="inner"><h1 class="logo"><a href="http://empscss.github.io/eMPSCSS/">eMPSCSS</a></h1><h2>&lt;e = MPSCSS&gt;</h2><a href="https://github.com/EMPSCSS/eMPSCSS" class="button"><small>View project on</small>GitHub</a><nav class="nav-main"><ul><li><a href="about.html">Test</a></li></ul></nav></div>');
+    
+});
